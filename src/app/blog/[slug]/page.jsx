542a71d0d72d45db21b4518f4a0d7d73c6bcb595
Matias <matiasweisz1@gmail.com>
2024-03-05 17:50:17 -0300

    +Tailwind- Blog Templates

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -2,9 +2,19 @@ import React from 'react'
 
 function SinglePostPage() {
   return (
-    <div>
-      Post Simple
+<main class="container mx-auto px-4 py-8">
+    <div class="flex flex-wrap -mx-4">
+        <div class="w-full lg:w-2/3 px-4">
+            <h2 class="text-xl font-bold mb-4">Main Content</h2>
+            <p class="text-gray-100">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris rhoncus ultrices
+                nunc, in sagittis nulla dapibus non. Sed convallis fermentum ante quis gravida. Morbi finibus lorem eu
+                eros consequat venenatis. Vestibulum at tristique neque. Fusce non sem auctor, sagittis arcu eget,
+                euismod nisl. Curabitur et bibendum sapien. Cras luctus suscipit magna ac bibendum. Vivamus eu semper
+                urna. Sed ullamcorper nisl vel dolor rhoncus facilisis. Donec vehicula nibh vitae est facilisis varius.
+            </p>
+        </div>
     </div>
+</main>
   )
 }
 
--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -3,7 +3,60 @@ import React from 'react'
 function BlogPage() {
   return (
     <div>
-      Pagina de Blog
+      <section class="text-gray-100 body-font overflow-hidden">
+  <div class="container px-5 py-24 mx-auto">
+    <div class="-my-8 divide-y-2 divide-gray-100">
+      <div class="py-8 flex flex-wrap md:flex-nowrap">
+        <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+          <span class="font-semibold title-font text-indigo-500">CATEGORIA</span>
+          <span class="mt-1 text-gray-100 text-sm">5 Marzo 2024</span>
+        </div>
+        <div class="md:flex-grow">
+          <h2 class="text-2xl font-medium text-indigo-500 title-font mb-2">Lorem Ipsum Lorem Ipsum Lorem Ipsum</h2>
+          <p class="leading-relaxed">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <a class="text-indigo-500 inline-flex items-center mt-4">Mas
+            <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
+              <path d="M5 12h14"></path>
+              <path d="M12 5l7 7-7 7"></path>
+            </svg>
+          </a>
+        </div>
+      </div>
+      <div class="py-8 flex flex-wrap md:flex-nowrap">
+        <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+          <span class="font-semibold title-font text-indigo-500">CATEGORIA</span>
+          <span class="mt-1 text-gray-100 text-sm">5 Marzo 2024</span>
+        </div>
+        <div class="md:flex-grow">
+          <h2 class="text-2xl font-medium text-indigo-500 title-font mb-2">Lorem Ipsum Lorem Ipsum Lorem Ipsum</h2>
+          <p class="leading-relaxed">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <a class="text-indigo-500 inline-flex items-center mt-4">Mas
+            <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
+              <path d="M5 12h14"></path>
+              <path d="M12 5l7 7-7 7"></path>
+            </svg>
+          </a>
+        </div>
+      </div>
+      <div class="py-8 flex flex-wrap md:flex-nowrap">
+        <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+          <span class="font-semibold title-font text-indigo-500">CATEGORIA</span>
+          <span class="text-sm text-gray-100">5 Marzo 2024</span>
+        </div>
+        <div class="md:flex-grow">
+          <h2 class="text-2xl font-medium text-indigo-500 title-font mb-2">Lorem Ipsum Lorem Ipsum Lorem Ipsum</h2>
+          <p class="leading-relaxed">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <a class="text-indigo-500 inline-flex items-center mt-4">Mas
+            <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
+              <path d="M5 12h14"></path>
+              <path d="M12 5l7 7-7 7"></path>
+            </svg>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
     </div>
   )
 }
